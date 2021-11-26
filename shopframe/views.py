e312095f794d47117924a197c9ecb39b90c44d95
sunhaozhepy <sunhaozhe050722a@qq.com>
2021-11-26 18:55:13 +0100

    second modification

--- shopframe/views.py
+++ shopframe/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from .models import Equipement, Animal
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import MoveForm
+from .models import Animal, Equipement
 
-def post_list(request):
+def animal_list(request):
     animals = Animal.objects.all()
-    return render(request, 'shopframe/post_list.html', {'animals': animals})
+    equipements = Equipement.objects.all()
+    return render(request, 'shopframe/animal_list.html', {'animals': animals, 'equipements': equipements})
+
+def animal_detail(request, id_animal):
+    animal = get_object_or_404(Animal, id_animal=id_animal)
+    lieu = animal.lieu
+    form = MoveForm()
+    if form.is_valid():
+        ancien_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+        ancien_lieu.disponibilite = "vacant"
+        ancien_lieu.save()
+        form.save()
+        nouveau_lieu = get_object_or_404(Equipement, id_equip=animal.lieu.id_equip)
+        nouveau_lieu.disponibilite = "occupied"
+        nouveau_lieu.save()
+        return redirect('animal_detail', id_animal=id_animal)
+    else:
+        form = MoveForm()
+        return render(request,
+                  'shopframe/animal_detail.html',
+                  {'animal': animal, 'lieu': lieu, 'form': form})
